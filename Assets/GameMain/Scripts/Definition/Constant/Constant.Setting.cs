b0d5ff5929a2916292843639ff8e87cfcd1acbaf
daxiong <daxiong>
2024-04-24 17:23:50 +0800

    本地化模块添加
    Signed-off-by: daxiong <daxiong>

---
+++ Assets/GameMain/Scripts/Definition/Constant/Constant.Setting.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+namespace GFLearning
+{
+    public static partial class Constant
+    {
+        public static class Setting
+        {
+            public const string Language = "Setting.Language"; //语言
+
+            ////声音组
+            //public const string SoundGroupMuted = "Setting.{0}Muted";
+            //public const string SoundGroupVolume = "Setting.{0}Volume";
+
+            ////音乐
+            //public const string MusicMuted = "Setting.MusicMuted";
+            //public const string MusicVolume = "Setting.MusicVolume";
+
+            ////音效
+            //public const string SoundMuted = "Setting.SoundMuted";
+            //public const string SoundVolume = "Setting.SoundVolume";
+
+            ////UI音效
+            //public const string UISoundMuted = "Setting.UISoundMuted";
+            //public const string UISoundVolume = "Setting.UISoundVolume";
+        }
+    }
+}
--- Assets/GameMain/Scripts/Procedure/ProcedureLaunch.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureLaunch.cs
@@ -18,6 +18,8 @@ namespace GFLearning
         protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
+
+            InitLocalization();//本地化数据加载
         }
 
         protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
@@ -34,5 +36,39 @@ namespace GFLearning
             base.OnLeave(procedureOwner, isShutdown);
         }
 
+        private void InitLocalization()
+        {
+            if (GameEntry.Base.EditorResourceMode && GameEntry.Base.EditorLanguage != Language.Unspecified)//编辑器模式且语言已被指定
+            {
+                // 编辑器资源模式直接使用 Inspector 上设置的语言
+                return;
+            }
+
+            Language language = GameEntry.Localization.Language; //获取当前的本地化语言
+            if (GameEntry.Setting.HasSetting(Constant.Setting.Language)) //检查设置中是否存在名为 Constant.Setting.Language 的设置项
+            {
+                try
+                {
+                    string languageString = GameEntry.Setting.GetString(Constant.Setting.Language);
+                    language = (Language)Enum.Parse(typeof(Language), languageString);//将字符串转换成相应的枚举值
+                }
+                catch
+                {
+
+                }
+            }
+
+            if (language != Language.English && language != Language.ChineseSimplified) //目前仅支持英语与简体中文
+            {
+                // 若是暂不支持的语言，则使用简体中文（默认）
+                language = Language.ChineseSimplified;
+
+                GameEntry.Setting.SetString(Constant.Setting.Language, language.ToString());//设置语言Constant.Setting.Language属性
+                GameEntry.Setting.Save();//保存语言设置
+            }
+
+            GameEntry.Localization.Language = language;
+            Log.Info("Init language settings complete, current language is '{0}'.", language.ToString());
+        }
     }
 }
